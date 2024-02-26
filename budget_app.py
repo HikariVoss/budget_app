a2daf01e206968b942028a60b4413392a215a15a
HikariVoss <hikarivoss.acc@gmail.com>
2024-02-26 23:04:11 +0000

    main functions laid out and title print functionality working

--- budget_app.py
+++ budget_app.py
@@ -1,32 +1,62 @@
+import math
 
 class Category:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, category) -> None:
+        self.category = category
+        self.transactions = []
+        self.balance = 0
+    def __str__(self) -> str:
+        self.string = ''
+        # format title
+        self.title_len = len(self.category)
+        self.white_space_left = math.floor((30 - self.title_len) / 2)
+        
+        self.white_space_right = math.ceil((30 - self.title_len) / 2)
+        
+        self.string = f'{self.num_to_char(self.white_space_left, "*")}{self.category}{self.num_to_char(self.white_space_right, "*")}'
+        for i in self.transactions: 
+            pass
+
+        return self.string
     def get_balance(self):
-        pass
+        return self.balance
+    
     def deposit(self, amount, description):
-        pass
-    def withdraw(self, amount, description):
-        pass
-    def check_balance(self):
-        pass
+        if self.check_balance(amount):
+            self.transactions.append([amount, description])
+    
+    def withdraw(self, amount, description = ''):
+        self.transactions.append([-amount, description])
+    
     def transfer(self, amount, destination):
         pass
-
+    
+    def check_balance(self, amount):
+        if (self.balance + amount) >= 0:
+            return True
+        else: return False
+    
+    def num_to_char(self, num, char = ' '):
+        self.chars = ''
+        for i in range(0, num):
+            self.chars += char
+        return self.chars
+    
 def create_spend_chart(categories):
     pass
 
-food = budget.Category("Food")
+
+food = Category("Food")
 food.deposit(1000, "initial deposit")
 food.withdraw(10.15, "groceries")
 food.withdraw(15.89, "restaurant and more food for dessert")
 
 print(food.get_balance())
-clothing = budget.Category("Clothing")
+clothing = Category("Clothing")
 food.transfer(50, clothing)
 clothing.withdraw(25.55)
 clothing.withdraw(100)
-auto = budget.Category("Auto")
+auto = Category("Auto")
 auto.deposit(1000, "initial deposit")
 auto.withdraw(15)
 
